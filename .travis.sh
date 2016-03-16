f3db8f5009e2139f6ecf9c8e573df254cbe30829
oglopss <oglopss@gmail.com>
2016-03-16 00:11:11 -0700

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -90,6 +90,7 @@ ss_build()
     CC=mipsel-unknown-linux-uclibc-gcc CXX=mipsel-unknown-linux-uclibc-g++ AR=mipsel-unknown-linux-uclibc-ar RANLIB=mipsel-unknown-linux-uclibc-ranlib ./configure --disable-ssp --host=mipsel-uclibc-linux --prefix=$HOME/ss-install --with-openssl=$HOME/openssl-install --host=mipsel-uclibc-linux --with-zlib=$HOME/zlib-install &> /dev/null
     make &> /dev/null
     make install &> /dev/null
+    local result=$?
     # local build_pid=$!
 
     # # Start a runner task to print a "still running" line every 5 minutes
