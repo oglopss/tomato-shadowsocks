cf5bb93c1f34e1d211fe4ea22114f79b48e80bd1
oglopss <oglopss@gmail.com>
2016-03-15 21:18:39 -0700

    Update update-gh-pages.sh

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -2,7 +2,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update gh-pages\n"
 
   #copy data we're interested in to other place
-  cp -R $HOME/ss-install/bin* $HOME/coverage
+  cp -R $HOME/ss-install/bin/*.tar.gz $HOME/coverage
 
   #go to home and setup git
   cd $HOME
@@ -10,7 +10,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.name "Travis"
 
   #using token clone gh-pages branch
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Uko/Rubidium-WHOIS.git  gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/oglopss/ctng-ss.git  gh-pages > /dev/null
 
   #go into diractory and copy data we're interested in to that directory
   cd gh-pages
