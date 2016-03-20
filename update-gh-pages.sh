dcb31c52f9869126dd037a42ff0a5f105253c842
oglopss <oglopss@gmail.com>
2016-03-19 18:28:59 -0700

    Update update-gh-pages.sh

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -9,19 +9,68 @@
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
-
+  
+  echo ===== about to clone ctng-ss-jekyllx ===============
+  echo 
+  x=$[ ( $RANDOM % 180 )  + 10 ]s
+  echo sleeping $x
+  sleep $x
+  
   #using token clone gh-pages branch
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/oglopss/ctng-ss.git  gh-pages > /dev/null
 
   #go into diractory and copy data we're interested in to that directory
   cd gh-pages
-  mkdir -p ss && cd ss
+  mkdir -p download && cd download
   cp -Rf $HOME/coverage/* .
 
   #add, commit and push files
   git add -f .
+  
+  # update ss.yml as well
+    echo ======== show $SS_VER =======
+  echo $SS_VER
+
+  # need to regenerate _data/ss.yml
+  cd ../_data
+
+  datetime=$(date '+%d/%m/%Y %H:%M:%S %Z');
+
+if grep -qe "build: $TRAVIS_BUILD_NUMBER$" ss.yml
+then
+    # code if found
+    # update files
+    if grep -qe "  - $SS_VER$" ss.yml
+    then
+        echo files already inside skip
+    else
+        cat >> ss.yml <<EOL
+  - $SS_VER
+EOL
+    fi
+  # update datetime
+  sed -ie 's@^date:\s.*@date: '"$datetime"'@g' ss.yml
+
+else
+    # code if not found
+    echo create new file
+
+
+    cat > ss.yml <<EOL
+build: $TRAVIS_BUILD_NUMBER
+date: $datetime
+files:
+  - $SS_VER
+EOL
+
+fi
+  
+  echo ============= print ss.yml =============
+  cat ./ss.yml
+  git add -f ss.yml
+  
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
 
-  echo -e "Done magic with coverage\n"
+  echo -e "Done magic with love\n"
 # fi
