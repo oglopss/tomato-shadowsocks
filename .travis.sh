c4e5834bdba75dc000cb66213bcbc22406a7a5c4
oglop <oglops@gmail.com>
2016-09-17 23:17:38 -0700

    no pcre

--- .travis.sh
+++ .travis.sh
@@ -181,7 +181,7 @@ ss_build()
 
     # eval "$config_cmd"
     # echo ======== this time is real ==========
-    CC=mipsel-unknown-linux-uclibc-gcc CXX=mipsel-unknown-linux-uclibc-g++ AR=mipsel-unknown-linux-uclibc-ar RANLIB=mipsel-unknown-linux-uclibc-ranlib ./configure --disable-ssp --host=mipsel-uclibc-linux --prefix=$HOME/ss-install --with-openssl=$HOME/openssl-install --with-zlib=$HOME/zlib-install $pcre_config
+    CC=mipsel-unknown-linux-uclibc-gcc CXX=mipsel-unknown-linux-uclibc-g++ AR=mipsel-unknown-linux-uclibc-ar RANLIB=mipsel-unknown-linux-uclibc-ranlib ./configure --disable-ssp --host=mipsel-uclibc-linux --prefix=$HOME/ss-install --with-openssl=$HOME/openssl-install --with-zlib=$HOME/zlib-install "$pcre_config"
 
 
     make > /dev/null 
