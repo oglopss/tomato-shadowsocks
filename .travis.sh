0601dd09151a03db3f23da8906c9666b64d4f096
oglop <oglops@gmail.com>
2017-02-12 19:37:23 -0800

    update

--- .travis.sh
+++ .travis.sh
@@ -387,7 +387,10 @@ ss_build()
         
         # echo --------
 
-        CPPFLAGS="-I$HOME/src/udns-$UDNS_VER -I$HOME/libev-install/include" LDFLAGS="-Wl,-rpath,/opt/lib:/lib:/usr/lib -L$HOME/src/udns-$UDNS_VER -L$HOME/libev-install/lib" CC=mipsel-unknown-linux-uclibc-gcc CXX=mipsel-unknown-linux-uclibc-g++ AR=mipsel-unknown-linux-uclibc-ar RANLIB=mipsel-unknown-linux-uclibc-ranlib ./configure --disable-ssp --prefix=$HOME/ss-install --with-pcre=$HOME/pcre-install --with-sodium=$HOME/libsodium-install --with-mbedtls=$HOME/mbedtls-install --host=mipsel-uclibc-linux
+        export CPPFLAGS="-I$HOME/src/udns-$UDNS_VER -I$HOME/libev-install/include" 
+        export LDFLAGS="-Wl,-rpath,/opt/lib:/lib:/usr/lib -L$HOME/src/udns-$UDNS_VER -L$HOME/libev-install/lib" 
+
+        CC=mipsel-unknown-linux-uclibc-gcc CXX=mipsel-unknown-linux-uclibc-g++ AR=mipsel-unknown-linux-uclibc-ar RANLIB=mipsel-unknown-linux-uclibc-ranlib ./configure --disable-ssp --prefix=$HOME/ss-install --with-pcre=$HOME/pcre-install --with-sodium=$HOME/libsodium-install --with-mbedtls=$HOME/mbedtls-install --host=mipsel-uclibc-linux
         
         echo -=-=-==-=-=-=-=-=-=-=
 
